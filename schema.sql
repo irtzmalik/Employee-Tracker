5eae20cd54fad1e8cb49530f8e9e563fde379b90
Irtaza Malik <irtz.malik@gmail.com>
2019-12-15 15:36:54 -0500

    edited schema & its order according to assignment

--- schema.sql
+++ schema.sql
@@ -2,18 +2,6 @@ DROP DATABASE IF EXISTS employee_DB;
 DROP Table IF EXISTS employee;
 CREATE DATABASE employee_DB;
 
-USE employee_DB;
-
-CREATE TABLE employee(
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT(10) NOT NULL,
-  manager_id INT(10),
-  PRIMARY KEY (id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id),
-    FOREIGN KEY(role_id) REFERENCES role(id)
-);
 DROP table IF EXISTS department;
 
 USE employee_DB;
@@ -33,6 +21,22 @@ CREATE TABLE role(
   salary DECIMAL(10,4) NOT NULL,
   department_id INT (10) NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY(department_id) REFERENCES department (id)
+  FOREIGN KEY(department_id) REFERENCES department (id) ON UPDATE CASCADE ON DELETE CASCADE
   
 );
+
+USE employee_DB;
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT(10) NOT NULL,
+  manager_id INT(10),
+  PRIMARY KEY (id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id),
+    FOREIGN KEY(role_id) REFERENCES role(id)  ON UPDATE CASCADE ON DELETE CASCADE
+ 
+);
+
+
